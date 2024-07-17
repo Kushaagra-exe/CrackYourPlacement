a3420069079387fcf1ed896ed3b10b8c93e5c3a0
Kushaagra <mehtakushaagra@gmail.com>
2024-07-17 23:54:09 +0530

    day2

---
+++ day2/MiddleoftheLinkedList.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class MiddleoftheLinkedList {
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head==null) return null;
+        ListNode fast = head, slow = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast= fast.next.next;
+        }
+        return slow;
+    }
+}
+}
